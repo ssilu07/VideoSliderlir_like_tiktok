646c1896c522440fd9cbe641260f72d78c0a1c0a
sumit <ssilu07@gmail.com>
2021-10-26 01:12:47 +0530

    Videoslider_Tiktok

---
+++ app/src/main/java/com/study/videoslider/member.java
@@ -0,0 +1,25 @@
+package com.study.videoslider;
+
+import java.lang.reflect.Member;
+
+public class member {
+    String title,url;
+
+    public member(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
